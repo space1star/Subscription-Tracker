6765e7400c63bf39a996de74a8e32a65eb6dd8d3
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:20:49 +0530

    Update subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -3,11 +3,10 @@ import { workflowClient } from "../config/upstash.js";
 import { SERVER_URL } from "../config/env.js";
 
 export const createSubscription = async (req, res, next) => {
-    try{
-        //create new subscription                                   
+    try{                                 
         const subscription = await Subscription.create({
             ...req.body, 
-            user: req.user._id, //user is set to
+            user: req.user._id,
         });
 
         const { workflowRunId } = await workflowClient.trigger({
@@ -28,23 +27,18 @@ export const createSubscription = async (req, res, next) => {
     }
 }
 
-//give us all subs. created by the user
 export const getUserSubscription = async(req, res, next) => {
     try{
-        if(req.user.id !== req.params.id){//check if user is same as the one in the token
+        if(req.user.id !== req.params.id){
             const error = new Error('You are not the owner of this account');
             error.status = 401;
             throw error;
         }
 
-        //rightful Owner
         const subscriptions = await Subscription.find({user: req.params.id});
 
-        //finally return
         res.status(200).json({success: true, data: subscriptions});
     }catch(e){
-        next(e);//forwarded over to error handling middleware
+        next(e);
     }
 }
-
-//export default createSubscription;
