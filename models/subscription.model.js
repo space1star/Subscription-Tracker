a2c971ff9827d6dbcbb64a16e1b2667316f328dd
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:27:39 +0530

    Update subscription.model.js

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
-//define the schema
 const subscriptionSchema = new mongoose.Schema(
     {
-        name:{//obj
+        name:{
             type: String,
             required: [true, 'Subscription name is req.'],
-            trim: true,//remove empty spaces
+            trim: true,
             minLength: 2,
             maxLength: 100,
         },
@@ -15,13 +14,11 @@ const subscriptionSchema = new mongoose.Schema(
             required: [true, 'Subscription price is req.'],
             min: [0, 'Price must be greater than 0'],
         },
-        //optional
         currency:{
             type: String,
             enum: ['INR', 'USD', 'EUR', 'GBP'],
             default: 'INR',
         },
-        //how often getting charged for subscription
         frequency:{
             type: String,
             enum: ['Daily', 'Weekly', 'Monthly', 'Yearly'],
@@ -41,21 +38,16 @@ const subscriptionSchema = new mongoose.Schema(
             enum: ['Active', 'Cancelled', 'Expired'],
             default: 'Active',
         },
-        //start date of subscription
         startDate:{
             type: Date,
             required: true,
-            //validate those properties,before they r used
             validate:{
-                //pass an obj
-                //where I define validator fun.
                 validator: (value) => value <= new Date(),
                 message: 'Start date must be in the past',
             }
         },
         renewalDate:{
             type: Date,
-            //required: true, ---> auto calculated
             validate:{
                 validator: function(value) {
                     return value > this.startDate;
@@ -64,12 +56,12 @@ const subscriptionSchema = new mongoose.Schema(
             }
         },
 
-        //user that is subscribed to the subs.,
+
         user:{//accepting the ID
             type: mongoose.Schema.Types.ObjectId,
             ref: 'User',
             required: true,
-            index: true,//Optimize the query by indexing user field
+            index: true,
         }
     },{timestamps: true}
 );
@@ -79,23 +71,14 @@ const subscriptionSchema = new mongoose.Schema(
 subscriptionSchema.pre('save', function(next){
     if(!this.renewalDate){
 
-        //define more periods
         const renewalPeriods = {
-            //obj
             Daily: 1,
             Weekly: 7,
             Monthly: 30,
             Yearly: 365,
         };
-        //set up renewal date
         this.renewalDate = new Date(this.startDate);
-        //adding no. of dated based on freq. we pass in
         this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriods[this.frequency]);
-        //e.g
-        //startDate = jan 1st
-        //monthly ---> frey
-        //30
-        //end on jan 31st (30+1)
     }
     if(this.renewalDate < new Date()){
         this.status = 'Expired';
@@ -104,6 +87,6 @@ subscriptionSchema.pre('save', function(next){
     next();
 });
 
-const Subscription = mongoose.model('Subscription', subscriptionSchema);//finally creating model out of above schema
+const Subscription = mongoose.model('Subscription', subscriptionSchema);
 
 export default Subscription;
