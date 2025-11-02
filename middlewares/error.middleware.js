9693920e989ce2fdb546be7f65b8efd0dfc95f98
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:25:08 +0530

    Update error.middleware.js

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,13 +1,9 @@
 const errorMiddleware = (err, req, res, next) => {
-//Create a subscription -> middleware(checks for renewal date) -> middleware(check for errors) -> next() -> controller
-//controller handles the actual logic of creating a subscription
     try{
-        let error = {...err};//obj
+        let error = {...err};
         error.message = err.message;
 
         console.error(err);
-
-        //types of errors -:
 
         //MONGOOSE BAD ObjectID error
         if(err.name === 'Cast Error'){
@@ -30,11 +26,10 @@ const errorMiddleware = (err, req, res, next) => {
         if(err.name === 'ValidationError'){
             const message = Object.values(err.errors).map(val => val.message);
 
-            error = new Error(message.join(', '));//join it based on comma and spaces
+            error = new Error(message.join(', '));
             error.statusCode = 400;
         }
-        //return the respond from this middleware
-        //500 -> general server error 
+
         res.status(error.statusCode || 500).json({success: false, error: error.message || 'Server Error'});
     } catch( error ){
         next(error);
