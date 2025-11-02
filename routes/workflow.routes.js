4f3d3454bd2cfaac8688cbedad98d8e3b676eefb
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:30:23 +0530

    Update workflow.routes.js

--- routes/workflow.routes.js
+++ routes/workflow.routes.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { sendReminders } from '../controllers/workflow.contoller.js';
 
-//initialize new workflow router
 const workflowRouter = Router();
 
-//define a route
 workflowRouter.post('/subscription/reminder', sendReminders);
 
 export default workflowRouter;
