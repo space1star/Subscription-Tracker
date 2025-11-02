1e373ba32676d389b0484fc32fe65769caa4d0d3
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:28:53 +0530

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from 'express';
 
 import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
-//import{signOut} from '../controllers/auth.controller.js';
 const authRouter = Router();
 
-// authRouter.post('/sign-up', (req, res) => {
-//     res.send({title: 'Sign up'});
-// });
-authRouter.post('/sign-up', signUp);//path: /api/v1/auth/sign-up (POST)
-
-//authRouter.post('/sign-in', (req, res) => res.send({title: 'Sign in'}));
+authRouter.post('/sign-up', signUp);
 authRouter.post('/sign-in', signIn);
-
 authRouter.post('/sign-out', signOut);
 
 export default authRouter;
