e3c058831753453dd866e814878c9881e3d75bff
space1star <166843017+space1star@users.noreply.github.com>
2025-11-02 22:29:34 +0530

    Update subscription.route.js

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -4,21 +4,17 @@ import { createSubscription,  getUserSubscription } from "../controllers/subscri
 
 const subscriptionRouter = Router();
 
-//dealing with CRUD functionalities(below)
 
 subscriptionRouter.get('/', (req, res) => res.send({title: 'GET all subscriptions'}));
 
-subscriptionRouter.get('/:id', (req, res) => res.send({title: 'GET subscription deatails'}));//subs. by id
+subscriptionRouter.get('/:id', (req, res) => res.send({title: 'GET subscription deatails'}));
 
-//subscriptionRouter.post('/', authorize, (req, res) => res.send({title: 'CREATE subscription'}));
 subscriptionRouter.post('/', authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => res.send({title: 'UPDATE subscription'}));//update subs. based on id
+subscriptionRouter.put('/:id', (req, res) => res.send({title: 'UPDATE subscription'}));
 
 subscriptionRouter.delete('/:id', (req, res) => res.send({title: 'DELETE subscription'}));
 
-//get all subscription belonging to a specific user
-//subscriptionRouter.get('/user/:id', (req, res) => res.send({title: 'GET all user subscriptions'}));
 subscriptionRouter.get('/user/:id', authorize, getUserSubscription);
 
 subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: 'CANCEL subscription'}));
